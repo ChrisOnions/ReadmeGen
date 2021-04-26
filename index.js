e7e6e370c77dd09e879d6567d0b923cda05c34c9
Chris Onions <c.onions101@gmail.com>
2021-04-26 15:33:56 +1000

    added Questions relating to the markdown

--- index.js
+++ index.js
@@ -2,26 +2,59 @@
 
 const inquirer = require("inquirer");
 const fs = require('fs');
-const generateMarkdown = require("./utils/generateMarkdown")
+const generateMarkdown = require("./utils/generateMarkdown");
+const Choice = require("inquirer/lib/objects/choice");
+const Choices = require("inquirer/lib/objects/choices");
 
 // TODO: Create an array of questions for user input
 const questions = [
   {
     type: 'input',
-    name: 'name',
-    message: 'Name of project',
-    choices: ['Built With', 'Aim', 'Project Description', 'User Story', 'Pictures :', 'Technologies', 'Collaborators']
+    name: 'Title',
+    message: 'Name of project?',
   },
   {
     type: 'input',
-    name: 'location',
-    message: 'Where are you from?',
+    name: 'licenece',
+    message: 'What licenece would you like to use?',
   },
   {
     type: 'input',
-    name: 'hobby',
-    message: 'What is your favorite hobby?',
-  }
+    name: 'Genereal Info',
+    message: 'Short description of the project?',
+  },
+  {
+    type: 'input',
+    name: 'Username',
+    message: 'github Username',
+
+  },
+  {
+    type: 'checkbox',
+    name: 'Built_with',
+    message: 'What did you use to build it ?',
+    Choices: ['Html', 'Css', 'ES6 Javascript', 'Node', 'NPM', 'Visual Studio Code', 'Reset.css']
+  },
+  {
+    type: 'input',
+    name: 'Aim',
+    message: 'What  does this project home to achieve',
+  },
+  {
+    type: 'input',
+    name: 'Description',
+    message: 'Project Description?',
+  },
+  {
+    type: 'input',
+    name: 'install instructions',
+    message: 'install instructions',
+  },
+  {
+    type: 'input',
+    name: 'pictures',
+    message: 'will the project be using pictures',
+  },
 ];
 
 // TODO: Create a function to write README file
@@ -34,9 +67,9 @@ function writeToFile(fileName, data) {
 // TODO: Create a function to initialize app
 function init() {
   inquirer.prompt(questions)
-  .then(data => {
-    writeToFile("README.md", generateMarkdown(data))
-  })
+    .then(data => {
+      writeToFile("README.md", generateMarkdown(data))
+    })
 }
 
 // Function call to initialize app
